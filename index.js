bb9a0e4820ab12b2a6966af4de5cbd9e01e11c06
Zach Brookman <zachbrookman48@gmail.com>
2021-04-15 18:45:06 -0400

    Completed Assingment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Zach',
+    streetAddress: '96 street'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value)  {
+       return Object.assign({},employee,{[key]: value })
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    return Object.assign(employee,{[key]: value })
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee.name; //why not employee.key??
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name; //you can choose the specific key? I thought the function should be able to delete name or streetAddress
+    return employee;
+}
+
